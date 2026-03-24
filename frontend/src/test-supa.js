54191a17238abe375dc0c4c324ec513bf25702a7
Ricardo Lima <ricardolimasouza27@gmail.com>
2026-03-23 22:53:08 -0300

    chore: remove test-supa files from repo

--- frontend/src/test-supa.js
+++
@@ -1,5 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabase = createClient(
-  'https://vnnvjoykfdqpbnjlebkh.supabase.co',
-  'eyJh... (eu vou pegar a chave do arquivo do usuario localmente)

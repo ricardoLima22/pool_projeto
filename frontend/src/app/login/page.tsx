2d20e539a3d1dd1cf772c4b0ca0bef2939d5c349
Ricardo Lima <ricardolimasouza27@gmail.com>
2026-03-25 16:01:42 -0300

    att

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -3,7 +3,7 @@ import LoginCard from "../../components/LoginCard";
 
 export default function Login() {
   return (
-    <div className="relative min-h-screen flex items-center justify-center overflow-hidden">
+    <div className="relative h-[100dvh] w-full flex flex-col justify-center overflow-hidden">
       <PoolBackground />
       <div className="relative z-10 w-full">
         <LoginCard />
